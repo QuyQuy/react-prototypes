2288002de132390f125fa9ce23c2d219ed938487
Quy Truong <qmtruong2@gmail.com>
2018-12-12 17:28:02 -0800

    react functional components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const luckyNumber = () => {
+    return Math.floor(Math.random() * 1000)
+}
+
+const greeting = (user) => {
+
+    return  <div>
+        <h1> hello my name is ${user.firstName} </h1>
+        <h2 className="text-muted"> The lucky number is ${user.luckyNumber} </h2>
+    </div>
+
+};
+
+const user = {
+    firstName: 'Quy',
+    luckyNumber: luckyNumber()
+
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
+
+
