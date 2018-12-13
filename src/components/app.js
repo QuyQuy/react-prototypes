00b788b9d6625c7f8109ddbf2d858d22dac9ae9a
Quy Truong <qmtruong2@gmail.com>
2018-12-12 20:02:02 -0800

    message

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,28 @@
+import React from 'react';
+import Table from './table';
 
-
+export default () => {
+    const students = [
+        {
+            name: 'kate',
+            course: 'communication',
+            grade: 100
+        },
+        {
+            name: 'nicole',
+            course: 'law',
+            grade: 89
+        },
+        {
+            name: 'shersh',
+            course: 'finance',
+            grade: 99
+        }
+    ];
+    return (
+        <div className='container'>
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
