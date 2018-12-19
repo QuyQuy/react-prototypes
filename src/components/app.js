cfa9f4d7f467240ba2094c5c70994bb446f70393
Quy Truong <qmtruong2@gmail.com>
2018-12-19 14:30:07 -0800

    input prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,36 @@
-import React from'react';
-import Stopwatch from './stopwatch';
+import React, { Component } from'react';
+import ContactList from './contact_list'
+import ContactForm from './contacts_forms'
+import contactData from '../data/contacts.js'
 
-export default (props) => {
-    return (
-        <div className='container'>
-            <h1 className='text-center'>Adress Book</h1>
-        <Stopwatch/>
-        </div>
 
-    )
+class App extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state= {
+            contacts: contactData
+        }
+    }
+    addContact = (contact) => {
+        console.log('App: addComponent:', contact);
+        this.setState={
+            contacts: [contact, ...this.state.contacts]
+        }
+    }
+    render() {
+        return (
+            <div className='container'>
+                <h1 className='text-center my-3'>Address Book</h1>
+                <div className="row">
+                    <div className='col-4'>
+                        <ContactForm add={this.addContact}/>
+                    </div>
+                    <ContactList contacts={this.state.contacts}/>
+                </div>
+            </div>
+
+        )
+    }
 }
+export default App;
---
+++ src/data/contacts.js
@@ -0,0 +1,22 @@
+
+
+export default [
+    {
+        firstName: 'Kate',
+        lastName: 'Guendel',
+        phone: 951-454-6325,
+        email: 'kateguendel@gmail.com'
+    },
+    {
+        firstName: 'Bruce',
+        lastName: 'Wayne',
+        phone: '201-725-5692',
+        email: 'theRealBatman@google.com'
+    },
+    {
+        firstName: 'Barry',
+        lastName: 'Allen',
+        phone: '319-210-7259',
+        email: 'theFlash@google.com'
+    }
+];
