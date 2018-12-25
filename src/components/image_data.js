3cc850cd2ef8863f778fa9bda21b3887d3a795f9
Quy Truong <qmtruong2@gmail.com>
2018-12-24 22:29:22 -0800

    react css prototype

---
+++ src/components/image_data.js
@@ -0,0 +1,84 @@
+import puppy1 from './images/animal-planet-1b513adcc779a5a1.jpg';
+import puppy2 from './images/download.jpeg';
+import puppy3 from './images/goldie.jpg';
+import puppy4 from './images/images.jpeg';
+import puppy5 from './images/images (1).jpeg';
+import puppy6 from './images/images (3).jpeg';
+import puppy7 from './images/images (2).jpeg';
+import puppy8 from './images/images (4).jpeg';
+import puppy9 from './images/images (5).jpeg';
+import puppy10 from './images/playing-fetch-cute-puppies-41539881-789-526.jpg';
+
+export default [
+    {
+        src: puppy1,
+        style: {
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: puppy2,
+        style: {
+            left: '40%',
+            transform: 'rotate(6deg)'
+        }
+    },
+    {
+        src: puppy3,
+        style: {
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
+    },
+    {
+        src: puppy4,
+        style: {
+            right: '8%',
+            top: '28%',
+            transform: 'rotate(8deg)'
+        }
+    },
+    {
+        src: puppy5,
+        style: {
+            left: '37%',
+            top: '25%',
+            transform: 'rotate(-12deg)'
+        }
+    },
+    {
+        src: puppy6,
+        style: {
+            left: '12%',
+            top: '26%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: puppy7,
+        style: {
+            right: '25%',
+            top: '48%',
+            transform: 'rotate(-5deg)'
+        }
+    },
+    {
+        src: puppy8,
+        style: {
+            right: '25%',
+            top: '52%',
+            transform: 'rotate(16deg)'
+        }
+    },
+    {
+        src: puppy9,
+        style: {
+
+        }
+    },
+    {
+        src: puppy10,
+        style: {}
+    }
+];
